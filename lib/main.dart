ba57a9c3123727380602f1ae48b04efc6d56e583
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-10-21 09:02:09 +0000

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,15 @@ import 'pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Stream movies = FirebaseFirestore.instance.collection('movies').snapshots();
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
