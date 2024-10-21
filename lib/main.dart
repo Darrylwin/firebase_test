0d439626c6ab93803e3463e1f1a822f36fc695bd
logossoudarryl20 <logossoudarryl20@gmail.com>
2024-10-21 08:12:15 +0000

    init firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
-fi
+import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
+import 'pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
